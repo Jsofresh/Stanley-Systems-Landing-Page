4e992edaa02e410beb6b8704fa1efab174f6b06c
Jaden Odorczuk <jsoforce@gmail.com>
2026-05-07 21:22:27 +0000

    deploy: enforce about redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ""
 
 const nextConfig = {
   basePath,
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
